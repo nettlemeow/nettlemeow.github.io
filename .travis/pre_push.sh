4ea896480934de8b0f2c94be164ff448b0151956
nettlemeow <nettlemeow@outlook.com>
2020-02-16 20:25:53 +0800

    change branch before data overwrite

---
+++ .travis/pre_push.sh
@@ -0,0 +1,8 @@
+
+change_branch() {
+  git checkout master
+}
+
+cd ./source/data
+
+change_branch
--- .travis/push.sh
+++ .travis/push.sh
@@ -4,7 +4,6 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout master
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
