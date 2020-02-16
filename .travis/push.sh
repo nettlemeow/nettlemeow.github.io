3c7c4130de282073690f645d787039d05e331687
nettlemeow <nettlemeow@outlook.com>
2020-02-16 19:21:39 +0800

    debug

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,6 +9,7 @@ commit_website_files() {
 }
 
 upload_files() {
+  echo ${GH_TOKEN}
   git remote add origin https://${GH_TOKEN}@github.com/nettlemeow/nettlemeow-data.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
