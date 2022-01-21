e04748ad0650b263abec5fb84a60a5f444e035a6
nettlemeow <nettlemeow@outlook.com>
2022-01-21 15:49:44 +0800

    fix: global key name error

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,7 +9,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add myorigin https://${secrets.PUSH_TOKEN}@github.com/nettlemeow/nettlemeow-data.git
+  git remote add myorigin https://${PUSH_TOKEN}@github.com/nettlemeow/nettlemeow-data.git
   git push --set-upstream myorigin master
 }
 
